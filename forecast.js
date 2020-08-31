650e3e2523ffa0ae21c5b3fc043eee241e60e1de
MiguelPacillo <mpaci006@fiu.edu>
2020-08-31 19:57:52 -0400

    Further work on logic

--- forecast.js
+++ forecast.js
@@ -1,18 +1,66 @@
 let apiKey = "uo2MGFBbCEhdFr6XBxNld7JW67byWPGO";
-let searchTerm = "Miami";
-let metric = "false";
+
+let metric = false;
 let unitTemp = "°F";
 let unitSpeed = "mph";
 let days = ["One", "Two", "Three", "Four", "Five"];
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+window.onload = function() { // Initializes search for default location on load
+    search();
+}
+
+function unitChangerFunction() { // Changes units on button click
+    if (!metric) {
+        metric = true;
+        unitTemp = "°C";
+        unitSpeed = "km/h";
+        document.getElementById("unitChanger").innerHTML = "Metric";
+    } else {
+        metric = false;
+        unitTemp = "°F";
+        unitSpeed = "mph";
+        document.getElementById("unitChanger").innerHTML = "Imperial";
+    }
+}
+
+function dayOfTheWeek(date) { // Gets the correct date for each forecast
+    let month = date.substring(5, 7);
+    let day = date.substring(8, 10);
+    let year = date.substring(0, 4);
+
+    let displayDate = month + "/" + day; // This returns the date like [month]/[day]
+
+    if (month.charAt(0) == "0") { // Cuts off the leading zero so we can map the correct month in the array
+        month = month.substring(1);
+    }
+
+    month = monthsArray[month - 1]; // Selects the corresponding month from the monthsArray
+
+    let forecastDate = new Date(month + " " + day + ", " + year);
+
+    let forecastWeekday = weekDays[forecastDate.getDay()]; // Selects the corresponding weekday from weekDays array
+
+    return forecastWeekday + " " + displayDate;
+}
 
 function search() { // Uses accuweather search API to get the location key
+
+    let searchTerm = document.getElementById("searchBar").value;
 
     fetch("http://dataservice.accuweather.com/locations/v1/search?apikey=" + apiKey + "&q=" + searchTerm + "&language=en-us&details=false&offset=0")
         .then(res => res.json())
         .then(data => {
             let searchKey = data[0].Key; // This returns the location key for the first location in the list
+
+            let locationName = data[0].EnglishName + ", " + data[0].Country.EnglishName;
+
+            document.getElementById("searchBar").value = locationName;
+            
             assignForcast(searchKey);
         })
+        .catch(() => alert("Error: Search term " + "\"" + searchTerm + "\"" + " is not a valid location"))
 }
 
 function assignForcast(searchKey) { // Uses the location key to find the forecast for that location
@@ -46,36 +94,21 @@ function assignForcast(searchKey) { // Uses the location key to find the forecas
                     forecastList[i].Temperature.Minimum.Value + unitTemp, // Min temp
                     forecastList[i].RealFeelTemperature.Maximum.Value + unitTemp, // Max real feel temp
                     forecastList[i].RealFeelTemperature.Minimum.Value + unitTemp, // Min real feel temp
-                    days[i] // Day number
+                    days[i], // Day number
+                    dayOfTheWeek(forecastList[i].Date) // Date like [weekday] [month]/[day]
                     ))
         }
-
-        console.log(assignList);
         
         for (let i = 0; i < assignList.length; i++) { // Calls display method for every day in the list
             assignList[i].displayValues();
         }
     })
+    .catch(() => alert("Server could not be reached."));
 }
-
-function unitChangerFunction() { // Changes units on button click
-    if (!metric) {
-        metric = true;
-        unitTemp = "°C";
-        unitSpeed = "km/h";
-        document.getElementById("unitChanger").innerHTML = "Metric";
-    } else {
-        metric = false;
-        unitTemp = "°F";
-        unitSpeed = "mph";
-        document.getElementById("unitChanger").innerHTML = "Imperial";
-    }
-}
-
 
 class Forecast {
     constructor(airQuality, dayWeather, dayPrecipitation, dayRain, daySnow, dayThunder, dayWindSpeed, dayWindDirection, dayIcon, nightWeather, nightPrecipitation, 
-                nightRain, nightSnow, nightThunder, nightWindSpeed, nightWindDirection, nightIcon, moonPhase, tempMax, tempMin, realFeelMax, realFeelMin, dayNumber) {
+                nightRain, nightSnow, nightThunder, nightWindSpeed, nightWindDirection, nightIcon, moonPhase, tempMax, tempMin, realFeelMax, realFeelMin, dayNumber, date) {
         
                     this.airQuality = airQuality;
 
@@ -107,10 +140,13 @@ class Forecast {
 
                     this.dayNumber = dayNumber;
 
+                    this.date = date;
+
     }
 
     displayValues() {
 
+        document.getElementById("date" + this.dayNumber).innerHTML = this.date; // Displays date for each forecast day
         document.getElementById("maxTemp" + this.dayNumber).innerHTML = this.tempMax; // Displays max temp value
         document.getElementById("minTemp" + this.dayNumber).innerHTML = this.tempMin; // Displays min temp value
         
